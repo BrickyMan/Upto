e1ff93f63cf63bcd9ecf1af56108f638cdfeb741
Bricksman <bricks.man@yahoo.com>
2020-09-01 23:11:31 +0300

    update

--- js/player.js
+++ js/player.js
@@ -1,7 +1,42 @@
-let player = document.querySelectorAll('.partnership-player_video');
-let playerOnBut = document.querySelectorAll('.partnership-player-play');
+let person = document.querySelectorAll('.feedback-inner-row-person');
+let personSwitchNext = document.querySelector('#switchNext');
+let personSwitchPrev = document.querySelector('#switchPrev');
+let personInfo = document.querySelector('#personInfo');
 
-playerOnBut[0].onclick = function() {
-    console.log('hello');
-    player[0].classList.toggle('partnership-player_video-full');
+let firstPerson = 0;
+let replacedPerson;
+
+let unitsInfo = [
+    ["url(images/photo1.jpg)", "Brand Kyle, Frontend-developer"],
+    ["url(images/photo2.jpg)", "Margo Brin, Tester"],
+    ["url(images/photo3.jpg)", "Jenifer Bushe, Backend-developer"],
+    ["url(images/photo4.jpg)", "Jeffer cruz, Ceo Ninja"],
+    ["url(images/photo5.jpg)", "Mike Jimson, Designer"],
+    ["url(images/photo6.jpg)", "John Smith, Product manager"],
+]
+
+personSwitchNext.onclick = function() {
+    replacedPerson = unitsInfo[5];
+    unitsInfo.pop();
+    unitsInfo.unshift(replacedPerson);
+    for (let i = 0; i < person.length; i++) {
+        person[i].style.backgroundImage = "";
+        person[i].style.backgroundImage = unitsInfo[i][0];
+        if (i == 4) {
+            personInfo.innerHTML = unitsInfo[3][1]
+        }
+    }
+}
+
+personSwitchPrev.onclick = function() {
+    replacedPerson = unitsInfo[0];
+    unitsInfo.shift();
+    unitsInfo.push(replacedPerson);
+    for (let i = 0; i < person.length; i++) {
+        person[i].style.backgroundImage = "";
+        person[i].style.backgroundImage = unitsInfo[i][0];
+        if (i == 4) {
+            personInfo.innerHTML = unitsInfo[3][1]
+        }
+    }
 }
